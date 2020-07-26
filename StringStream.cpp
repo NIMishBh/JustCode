b1df60e03278d0bc6e9c9c3e7546525c21cafa51
NIMishBh <nimish864@gmail.com>
2020-07-26 19:42:19 +0530

    Initially

---
+++ StringStream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	vector<int> arr;
+    stringstream sstr(str);
+    char ch;
+    int samp;
+    while(sstr)
+    {
+        sstr>>samp>>ch;
+        arr.push_back(samp);
+    }
+    return arr;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
